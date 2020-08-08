9d66708f802b6a6f86665fb21dbce4636618ac7e
Zachary Romsdahl <zdromsdahl@gmail.com>
2020-08-08 00:50:05 -0700

    Created a simple express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
 // Entry point of express server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + '/../public'))
+
+app.listen(port, () => {
+  console.log(`Pictures server listening on port ${port}`)
+})
