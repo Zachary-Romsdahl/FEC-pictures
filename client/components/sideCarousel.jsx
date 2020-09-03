b69c4c5bb1a4fa34b5bff04fa9fd837e1a7c32f6
Zachary Romsdahl <zdromsdahl@gmail.com>
2020-09-03 02:40:35 -0700

    Working on Carousel functionality

--- client/components/sideCarousel.jsx
+++ client/components/sideCarousel.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+
 const Div = styled.div`
   overflow:hidden;
 `;
@@ -19,18 +20,41 @@ const Image = styled.img`
   height: 60px;
   width: 60px;
   border-radius: 6px;
+  opacity: 0.6;
+`;
+
+const ClickedListItem = styled.li`
+height: 56px;
+width: 56px;
+margin: 0px 6px 6px 0px;
+border: 2px solid #000;
+border-radius: 6px;
+`;
+
+const ClickedImage = styled.img`
+  height:56px;
+  width:56px;
+  border-radius: 6px;
+  opacity: 1;
 `;
 
 function SideCarousel(props) {
-  const { pictures } = props;
+  const { pictures, currPicPos } = props;
   return (
     <Div>
       <List>
         {pictures.map((picture, i) => (
-          <ListItem key={i}>
-            <Image src={picture.thumbnail} alt={picture.thumbnail} />
-          </ListItem>
-        ))}
+          (i === currPicPos)
+            ? (
+              <ClickedListItem key={i}>
+                <ClickedImage src={picture.thumbnail} alt={picture.thumbnail} />
+              </ClickedListItem>
+            )
+            : (
+              <ListItem key={i}>
+                <Image src={picture.thumbnail} alt={picture.thumbnail} />
+              </ListItem>
+            )))}
       </List>
     </Div>
   );
